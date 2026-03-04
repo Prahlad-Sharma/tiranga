85b0e0bc90a524bc4f542bff95b520c47f08ca21
Prahlad-Sharma <prahladiam@gmail.com>
2026-03-04 19:36:28 +0530

    api 1

--- src/controllers/trxWingoController.js
+++ src/controllers/trxWingoController.js
@@ -704,7 +704,7 @@ const addTrxWingo = async (game) => {
       } else {
         let response = await axios({
           method: "GET",
-          url: "https://api.trongrid.io/v1/blocks?limit=20",
+          url: "https://apilist.tronscanapi.com/api/block?sort=-balance&start=0&limit=20&producer=&number=&start_timestamp=&end_timestamp=",
           headers: {
             "TRON-PRO-API-KEY": process.env.TRON_API_KEY,
           },
